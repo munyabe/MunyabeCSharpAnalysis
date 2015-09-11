818455013de4a848728c7d94baed17cf45f27cae
munyabe <yo.betek@gmail.com>
2015-09-11 12:21:36 +0900

    Add AvoidEnumHasFlagAnalyzer.

---
+++ MunyabeCSharpAnalysis/MunyabeCSharpAnalysis.Test/Analyzers/Performance/AvoidEnumHasFlagAnalyzerTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Munyabe.CSharp.Analysis.Analyzers.Performance;
+using TestHelper;
+
+namespace Munyabe.CSharp.Analysis.Test.Analyzers.Performance
+{
+    /// <summary>
+    /// <see cref="AvoidEnumHasFlagAnalyzer"/>をテストするクラスです。
+    /// </summary>
+    [TestClass]
+    public class AvoidEnumHasFlagAnalyzerTest : CodeFixVerifier
+    {
+        [TestMethod]
+        public void AnalyzeViolation()
+        {
+            var test = @"using System;
+
+namespace AnalyzerSample.Test.Performance
+{
+    class AvoidEnumHasFlagAnalyzerTestTarget
+    {
+        public bool Method()
+        {
+            return TestEnum.Hoge.HasFlag(TestEnum.Fuga);
+        }
+    }
+
+    [Flags]
+    enum TestEnum
+    {
+        Hoge,
+        Fuga
+    }
+}";
+
+            var expected = new DiagnosticResult
+            {
+                Id = AvoidEnumHasFlagAnalyzer.DiagnosticId,
+                Message = string.Format("Avoid Enum.HasFlag prefer bit operator"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[]
+                {
+                    new DiagnosticResultLocation("Test0.cs", 9, 20)
+                }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        /// <inheritdoc />
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AvoidEnumHasFlagAnalyzer();
+        }
+    }
+}
---
+++ MunyabeCSharpAnalysis/MunyabeCSharpAnalysis/Analyzers/Performance/AvoidEnumHasFlagAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Munyabe.CSharp.Analysis.Analyzers.Performance
+{
+    /// <summary>
+    /// <see cref="Enum.HasFlag"/>の呼び出しを検出するルールです。
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AvoidEnumHasFlagAnalyzer : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// ルールの識別子です。
+        /// </summary>
+        public const string DiagnosticId = "Performance.AvoidEnumHasFlag";
+
+        /// <summary>
+        /// ルールの説明です。
+        /// </summary>
+        private static DiagnosticDescriptor _descriptor = new DiagnosticDescriptor(
+            DiagnosticId,
+            "Avoid Enum.HasFlag",
+            "Avoid Enum.HasFlag prefer bit operator",
+            "Performance",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        /// <inheritdoc />
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_descriptor);
+
+        /// <inheritdoc />
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
+        }
+
+        /// <summary>
+        /// メソッドを実行するノードを解析します。
+        /// </summary>
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var node = context.Node;
+
+            var methodSymbol = context.SemanticModel.GetSymbolInfo(node).Symbol as IMethodSymbol;
+            if (IsEnumHasFlag(methodSymbol))
+            {
+                var diagnostic = Diagnostic.Create(_descriptor, node.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="Enum.HasFlag"/>の呼び出しかどうかを判定します。
+        /// </summary>
+        private static bool IsEnumHasFlag(IMethodSymbol symbol)
+        {
+            return symbol != null
+                && symbol.Name == "HasFlag"
+                && symbol.ContainingType.SpecialType == SpecialType.System_Enum
+                && !symbol.IsStatic
+                && symbol.MethodKind == MethodKind.Ordinary;
+        }
+    }
+}
