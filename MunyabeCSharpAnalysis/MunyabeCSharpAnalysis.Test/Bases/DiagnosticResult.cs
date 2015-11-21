f056efdc1bc3ea9c8de055a21c4a0ebd78713c77
munyabe <yo.betek@gmail.com>
2015-11-21 20:38:49 +0900

    Separate classes in the other file.

--- MunyabeCSharpAnalysis/MunyabeCSharpAnalysis.Test/Bases/DiagnosticResult.cs
+++ MunyabeCSharpAnalysis/MunyabeCSharpAnalysis.Test/Bases/DiagnosticResult.cs
@@ -1,52 +1,7 @@
-﻿using System;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 
 namespace Munyabe.CSharp.Analysis.Test.Bases
 {
-    /// <summary>
-    /// ソースコードの位置を表す構造体です。
-    /// </summary>
-    public struct DiagnosticResultLocation
-    {
-        /// <summary>
-        /// 列数を取得します。
-        /// </summary>
-        public int Column { get; }
-
-        /// <summary>
-        /// 行数を取得します。
-        /// </summary>
-        public int Line { get; }
-
-        /// <summary>
-        /// ファイルパスを取得します。
-        /// </summary>
-        public string Path { get; }
-
-        /// <summary>
-        /// インスタンスを初期化します。
-        /// </summary>
-        /// <param name="path">ファイルパス</param>
-        /// <param name="line">行数</param>
-        /// <param name="column">列数</param>
-        public DiagnosticResultLocation(string path, int line, int column)
-        {
-            if (line < -1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(line), "line must be >= -1");
-            }
-
-            if (column < -1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(line), "column must be >= -1");
-            }
-
-            Path = path;
-            Line = line;
-            Column = column;
-        }
-    }
-
     /// <summary>
     /// ソースコードの解析結果を格納する構造体です。
     /// </summary>
---
+++ MunyabeCSharpAnalysis/MunyabeCSharpAnalysis.Test/Bases/DiagnosticResultLocation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Munyabe.CSharp.Analysis.Test.Bases
+{
+    /// <summary>
+    /// ソースコードの位置を表す構造体です。
+    /// </summary>
+    public struct DiagnosticResultLocation
+    {
+        /// <summary>
+        /// 列数を取得します。
+        /// </summary>
+        public int Column { get; }
+
+        /// <summary>
+        /// 行数を取得します。
+        /// </summary>
+        public int Line { get; }
+
+        /// <summary>
+        /// ファイルパスを取得します。
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// インスタンスを初期化します。
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <param name="line">行数</param>
+        /// <param name="column">列数</param>
+        public DiagnosticResultLocation(string path, int line, int column)
+        {
+            if (line < -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(line), "line must be >= -1");
+            }
+
+            if (column < -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(line), "column must be >= -1");
+            }
+
+            Path = path;
+            Line = line;
+            Column = column;
+        }
+    }
+}
